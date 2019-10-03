acdf52200c2214522cfb19106030d579f63a0708
Will Eccles <ghostyx123@gmail.com>
2019-10-03 14:18:05 -0400

    added next_sequence

---
+++ README.txt
@@ -0,0 +1,13 @@
+WHAT IS THIS?
+
+This is a header-only parser for the GTF file format. If you are unaware of
+what that is, then this is almost certainly not meant for you.
+
+BUILDING
+
+This program assumes a modern C++ compiler, preferrably compatible with C++17.
+
+LICENSE
+
+This software is released under the Apache License version 2.0.
+See the LICENSE file for details.
--- gtf.h
+++ gtf.h
@@ -26,27 +26,29 @@
 #ifndef _GTF_H
 #define _GTF_H
 
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <stdexcept>
-#include <map>
 #include <cstdlib>
+#include <fstream>
+#include <map>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
 
 #define NO_SCORE std::numeric_limits<double>::infinity
 
 struct GTFSequence {
-    std::string seqname;
-    std::string source;
-    std::size_t start;
-    std::size_t end;
-    double      score;
-    char        strand;
-    int         frame;
+    std::string seqname;    // sequence name
+    std::string source;     // source
+    std::size_t start;      // start
+    std::size_t end;        // end
+    double      score;      // either a score or NO_SCORE if a .
+    char        strand;     // + or -
+    short       frame;      // frame (0, 1, 2)
 
-    std::map<std::string, std::string> attributes;
+    std::map<std::string, std::string> attributes;  // any attributes to use
 
-    bool has_attribute(const std::string& attr) {
+    bool has_attribute(const std::string& attr) const noexcept {
         return (attributes.find(attr) != attributes.end());
     }
 };
@@ -59,8 +61,72 @@ class GTFFile {
             }
         }
 
+        // Gets the next sequence and returns true or returns false if none
+        // are left to be gotten.
+        bool next_sequence(GTFSequence& out_seq) const {
+            std::string line;
+            std::stringstream ss;
+            while (std::getline(infile, line)) {
+                sanitize_line(line);
+                if (!valid_line(line)) {
+                    continue;
+                } else {
+                    ss = std::stringstream(line);
+                    std::string tmpscore;
+                    ss >> out_seq.seqname
+                        >> out_seq.source
+                        >> out_seq.start
+                        >> out_seq.end
+                        >> tmpscore // check this afterwards
+                        >> out_seq.strand
+                        >> out_seq.frame;
+                    if (tmpscore == ".") {
+                        out_seq.score = NO_SCORE;
+                    } else {
+                        out_seq.score = std::atof(tmpscore);
+                    }
+
+                    out_seq.attributes.clear();
+                    // get the attributes if any
+                    std::string tmpattr, tmpkey, tmpval;
+                    while (std::getline(ss, tmpattr, ';')) {
+                        std::getline(tmpattr, tmpkey, '\t');
+                        std::getline(tmpattr, tmpval);
+                        out_seq.attributes.insert(tmpattr, tmpval);
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     private:
         std::ifstream infile;
+
+        const static std::basic_regex valid_line_regex("^\\S+\\t\\S+\\t\\S+\\t\\d+\\t\\d+\\t\\S+\\t\\S+\\t\\d(\\t\\S+\\t\\S+)*$");
+        
+        static inline std::string& sanitize_line(std::string& line) {
+            // get rid of comments
+            // TODO maybe make this not find '#' characters that are in strings
+            if (line.find_first_of('#') != line.npos()) {
+                line.erase(line.find_first_of('#'), line.length() - 1);
+            }
+            
+            // trim the right side
+            line.erase(line.find_last_not_of(" \t") + 1);
+
+            // trim the left side
+            line.erase(0, line.find_first_not_of(" \t"));
+
+            // we sanitized the line in place, but return a reference anyway
+            return line;
+        }
+
+        static inline bool valid_line(const std::string& line) {
+            return std::regex_search(line, valid_line_regex);
+        }
 };
 
 #endif /* _GTF_H */
